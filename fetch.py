da4d4e884f52f219f4aeb4d6a0ef75776567979d
debrigand <gcauwenberg+github@gmail.com>
2019-09-18 22:34:17 +0200

    PEP8 fixes
    No functional changes, just ran
    Black, the uncompromising code formatter.

--- fetch.py
+++ fetch.py
@@ -4,46 +4,51 @@ import MySQLdb as mdb
 from datetime import date, datetime
 import sys
 
-dbhost = 'localhost'
-dbuser = '<TO BE SET>'
-dbpassword = '<TO BE SET>'
-dbname = '<TO BE SET>'
-inverter = '<TO BE SET>'
+dbhost = "<TO BE SET>"
+dbuser = "<TO BE SET>"
+dbpassword = "<TO BE SET>"
+dbname = "<TO BE SET>"
+inverter = "<TO BE SET>"
 
 # Read inverter data
 def read_inverter(client):
     status = client.get_status()
-    inverter = status.inverters.primary   
+    inverter = status.inverters.primary
     data = {}
-    data['temp'] = inverter.temperature.value
-    data['serial'] = inverter.dspSn
-    data['power'] = status.powerWatt
-    data['e_day'] = status.energy.today
-    data['e_total'] = status.energy.total
-    data['u_ac'] = status.voltage
-    data['u_dc'] = inverter.voltage
+    data["temp"] = inverter.temperature.value
+    data["serial"] = inverter.dspSn
+    data["power"] = status.powerWatt
+    data["e_day"] = status.energy.today
+    data["e_total"] = status.energy.total
+    data["u_ac"] = status.voltage
+    data["u_dc"] = inverter.voltage
     return data
+
 
 def read_optimizer(opt):
     data = {}
-    data['serial'] = opt.serialNumber
+    data["serial"] = opt.serialNumber
     t = opt.lastReport
-    data['timestamp'] = "{0}-{1}-{2} {3}:{4}:{5}".format(t.year, t.month, t.day, t.hour, t.minute, t.second)
-    data['u_out'] = opt.outputV
-    data['u_in'] = opt.inputV
-    data['temp'] = opt.temperature.value
+    data["timestamp"] = "{0}-{1}-{2} {3}:{4}:{5}".format(
+        t.year, t.month, t.day, t.hour, t.minute, t.second
+    )
+    data["u_out"] = opt.outputV
+    data["u_in"] = opt.inputV
+    data["temp"] = opt.temperature.value
     return data
+
 
 def read_optimizers(client):
     maintenance = client.get_maintenance()
     data = {}
     optimizers = maintenance.diagnostics.inverters.primary.optimizer
     for opt in optimizers:
-        if (opt.online):
+        if opt.online:
             opt_data = read_optimizer(opt)
-            data[str(opt_data['serial'])] = opt_data
+            data[str(opt_data["serial"])] = opt_data
     return data
-   
+
+
 def connect_inverter():
     try:
         return SolarEdge("http://{0}".format(inverter))
@@ -51,12 +56,14 @@ def connect_inverter():
         print("Can't connect to inverter - check network settings\n")
         sys.exit(1)
 
+
 def connect_database():
     try:
-        return mdb.connect(dbhost, dbuser, dbpassword, dbname);
+        return mdb.connect(dbhost, dbuser, dbpassword, dbname)
     except mdb.MySQLError:
         print("Can't connect to database - check database settings\n")
         sys.exit(1)
+
 
 # Retrieves the energy generated since the
 # previous run
@@ -68,19 +75,20 @@ def get_energy_delta(dbh, inv):
     try:
         cursor.execute(sql)
         (E_day_prev, updated) = cursor.fetchone()
-    except TypeError: # When database is empty
-        return inv['e_day']
+    except TypeError:  # When database is empty
+        return inv["e_day"]
     except mdb.MySQLError as e:
         print("Error selecting last inverter data: {0}".format(e))
         sys.exit(1)
 
     if updated.date() != date.today():
-        delta = inv['e_day']
+        delta = inv["e_day"]
     else:
-        delta = inv['e_day'] - E_day_prev
+        delta = inv["e_day"] - E_day_prev
 
     print("Since last run we produced {:7.2f} Wh".format(delta))
     return delta
+
 
 # Fetch for each optimizer the last recorded data
 def get_last_optimizer_data(dbh):
@@ -100,32 +108,34 @@ def get_last_optimizer_data(dbh):
 
     return result
 
+
 # Distribute the energy reported by the inverter over the optimizers
 # Of course this is at best an aproximation
 def calculate_energy(dbh, inv, optimizers):
-    inv['i_dc'] = inv['power'] / inv['u_dc']
+    inv["i_dc"] = inv["power"] / inv["u_dc"]
 
     E_delta = get_energy_delta(dbh, inv)
 
     U_total = 0
     for key in optimizers:
         opt = optimizers[key]
-        U_total += opt['u_out']
+        U_total += opt["u_out"]
 
     last = get_last_optimizer_data(dbh)
     for row in last:
         (serial, updated, u_out, e_day, e_total) = row
         try:
             opt = optimizers[str(serial)]
-            opt_delta = opt['u_out'] / U_total * E_delta
-            opt['e_total'] = e_total + opt_delta
+            opt_delta = opt["u_out"] / U_total * E_delta
+            opt["e_total"] = e_total + opt_delta
 
             if updated.date() == date.today():
-                opt['e_day'] = e_day + opt_delta
+                opt["e_day"] = e_day + opt_delta
             else:
-                opt['e_day'] = opt_delta
+                opt["e_day"] = opt_delta
         except KeyError:
             pass
+
 
 # Save the data for inverter and optimizers in the database
 def save_data(dbh, inv, optimizers, now):
@@ -138,28 +148,67 @@ def save_data(dbh, inv, optimizers, now):
 
     cursor = dbh.cursor()
     try:
-        cursor.execute(sql1, (now, inv['power'], inv['u_ac'], inv['u_dc'], inv['i_dc'], inv['e_day'], inv['e_total' ], inv['temp']))
+        cursor.execute(
+            sql1,
+            (
+                now,
+                inv["power"],
+                inv["u_ac"],
+                inv["u_dc"],
+                inv["i_dc"],
+                inv["e_day"],
+                inv["e_total"],
+                inv["temp"],
+            ),
+        )
     except mdb.MySQLError as e:
         print("Error inserting inverter data: {0}".format(e))
 
     for key in optimizers:
         opt = optimizers[key]
         try:
-            cursor.execute(sql2, (now, opt['serial'], opt['timestamp'], opt['u_out']*inv['i_dc'], opt['u_out'], opt['u_in'], opt['e_day'], opt['e_total'], opt['temp']))
-        except KeyError: # When database empty
-            cursor.execute(sql2, (now, opt['serial'], opt['timestamp'], opt['u_out']*inv['i_dc'], opt['u_out'], opt['u_in'], 0, 0, opt['temp']))
+            cursor.execute(
+                sql2,
+                (
+                    now,
+                    opt["serial"],
+                    opt["timestamp"],
+                    opt["u_out"] * inv["i_dc"],
+                    opt["u_out"],
+                    opt["u_in"],
+                    opt["e_day"],
+                    opt["e_total"],
+                    opt["temp"],
+                ),
+            )
+        except KeyError:  # When database empty
+            cursor.execute(
+                sql2,
+                (
+                    now,
+                    opt["serial"],
+                    opt["timestamp"],
+                    opt["u_out"] * inv["i_dc"],
+                    opt["u_out"],
+                    opt["u_in"],
+                    0,
+                    0,
+                    opt["temp"],
+                ),
+            )
         except mdb.MySQLError as e:
             code, msg = e.args
             print("Error inserting optimizer data: code {0}, msg {1}".format(code, msg))
     dbh.commit()
     return
 
+
 now = datetime.now()
 print("Running at {0}".format(now))
 client = connect_inverter()
 inverter_data = read_inverter(client)
 optimizer_data = read_optimizers(client)
-if inverter_data['power'] > 0:
+if inverter_data["power"] > 0:
     db = connect_database()
     calculate_energy(db, inverter_data, optimizer_data)
     save_data(db, inverter_data, optimizer_data, now)
--- upload.py
+++ upload.py
@@ -4,19 +4,21 @@ from datetime import date, datetime
 import requests
 import sys
 
-dbhost = 'localhost'
-dbuser = '<TO BE SET>'
-dbpassword = '<TO BE SET>'
-dbname = '<TO BE SET>'
+dbhost = "localhost"
+dbuser = "<TO BE SET>"
+dbpassword = "<TO BE SET>"
+dbname = "<TO BE SET>"
 
-pvoutput_api='<TO BE SET>'
+pvoutput_api = "<TO BE SET>"
+
 
 def connect_database():
     try:
-        return mdb.connect(dbhost, dbuser, dbpassword, dbname);
+        return mdb.connect(dbhost, dbuser, dbpassword, dbname)
     except mdb.MySQLError:
         print("Can't connect to database - check database settings\n")
         sys.exit(1)
+
 
 def get_systems(dbh):
     sql = """SELECT pvo_systemid, pvo_last_live from live_update"""
@@ -31,9 +33,10 @@ def get_systems(dbh):
 
     return result
 
+
 def get_last_output_data(dbh, system):
     sql = """SELECT updated, sum(E_day), sum(power), max(temp)
-             FROM optimizer JOIN layout ON layout.serial = optimizer.serial 
+             FROM optimizer JOIN layout ON layout.serial = optimizer.serial
              WHERE pvo_systemid = %s  AND updated > %s
              GROUP BY updated
              ORDER BY updated
@@ -49,6 +52,7 @@ def get_last_output_data(dbh, system):
 
     return result
 
+
 def create_csv(data):
     rows = []
     for output in data:
@@ -56,9 +60,11 @@ def create_csv(data):
             output[0].strftime("%Y%m%d"),
             output[0].strftime("%H:%M"),
             str(int(output[1])),
-            str(int(output[2])))
+            str(int(output[2])),
+        )
         rows.append(",".join(row))
     return ";".join(rows)
+
 
 # Save the last updated timestamp in the live_update table
 def save_last(dbh, system, last):
@@ -75,27 +81,32 @@ def save_last(dbh, system, last):
     dbh.commit()
     return
 
+
 def find_last(data):
     row = data[-1]
     return row[0]
 
+
 def pvoutput_update(system, output):
     headers = {
-       "X-Pvoutput-Apikey": str(pvoutput_api),
-       "X-Pvoutput-systemid": str(system[0])
+        "X-Pvoutput-Apikey": str(pvoutput_api),
+        "X-Pvoutput-systemid": str(system[0]),
     }
 
-    data = {
-        'data': output,
-    }
+    data = {"data": output}
 
-    response = requests.post('https://pvoutput.org/service/r2/addbatchstatus.jsp', headers=headers, data=data)
+    response = requests.post(
+        "https://pvoutput.org/service/r2/addbatchstatus.jsp",
+        headers=headers,
+        data=data
+    )
 
-    if (response.status_code != 200):
+    if response.status_code != 200:
         print("Received status code {}, {}".format(response.status_code, response.text))
         return False
 
     return True
+
 
 now = datetime.now()
 print("Running at {0}".format(now))
@@ -105,7 +116,11 @@ for system in systems:
     data = get_last_output_data(db, system)
     if len(data) > 0:
         csv = create_csv(data)
-        print("Uploading {0} results to pvupload for system {1}".format(len(data), system[0]))
+        print(
+            "Uploading {0} results to pvupload for system {1}".format(
+                len(data), system[0]
+            )
+        )
         res = pvoutput_update(system, csv)
         if res:
             last = find_last(data)
